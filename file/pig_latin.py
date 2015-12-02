cd306c4d0f5cf4005a6c60cda3c260528e1c87da
sphantix <sphantix@gmail.com>
2015-12-02 09:06:25 +0800

    Develop | [FILE] Complete pig latin

--- file/pig_latin.py
+++ file/pig_latin.py
@@ -2,18 +2,46 @@
 # -*- coding: UTF-8 -*-
 # author : sphantix
 
-def pig_latin (s):
-    vowel = 'AaEeIiOoUu'
-    for i, c in s:
-        if c in vowel:
-            print c, '%d' %i
+VOWELS = ('A','a','E','e','I','i','O','o','U','u')
+
+def convert_word (word):
+    i = 0
+    for c in word:
+        if c not in VOWELS:
+            i+=1
             continue
         else:
-            outs = s[i:] + '-' + s[:i] + 'ay'
+            outs = word[i:] + '-' + word[:i] + 'ay'
             break
 
     return outs
 
+def convert_sentence(sentence):
+    words = sentence.split(' ')
+    new_sentence = ""
+    for word in words:
+        new_sentence = new_sentence + convert_word(word)
+        new_sentence = new_sentence + " "
+    return new_sentence.strip()
 
-print pig_latin("three")
+def convert_word_back(word):
+    words = word.split('-')
+    new_word = ''.join([words[1][:-2], words[0]])
+    return new_word
 
+def convert_sentence_back(sentence):
+    words = sentence.split(' ')
+    new_sentence = ""
+    for word in words:
+        new_sentence = new_sentence + convert_word_back(word)
+        new_sentence = new_sentence + " "
+    return new_sentence
+
+origin_sentence = "Now, let's write the main program code, to ask the user and convert."
+
+converted_sentence = convert_sentence(origin_sentence)
+converted_back_sentence = convert_sentence_back(converted_sentence)
+
+print "origin_sentence = %s" %origin_sentence
+print "converted_sentence = %s" %converted_sentence
+print "converted_back_sentence = %s" %converted_back_sentence
